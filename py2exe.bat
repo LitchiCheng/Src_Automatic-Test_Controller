8d8499829ee8bf267f03dff6c18d3c790bf7a70c
darboy <974782852@qq.com>
2019-11-26 09:33:17 +0800

    1.修复DI检测 2.修改编译批处理

--- py2exe.bat
+++ py2exe.bat
@@ -1,2 +1,7 @@
 pyinstaller -D -w --hidden-import=PyQt5.sip main.py
+xcopy .\icon dist\main\icon /E /Y /I
+xcopy .\background dist\main\background /E /Y /I
+xcopy .\report dist\main\report /E /Y /I
+xcopy .\code_image dist\main\code_image /E /Y /I
+copy DejaVuSansMono.ttf dist\main /Y
 pause
